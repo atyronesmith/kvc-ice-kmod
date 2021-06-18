09cddd88aa1ae0c4abb0aa6261e63bbbfe5c747b
Aaron Smith <aasmith@redhat.com>
2021-06-17 21:03:08 -0400

    fixe ko

--- ice-kmod-lib.sh
+++ ice-kmod-lib.sh
@@ -123,13 +123,14 @@ load_kmods() {
     echo "Loading kernel modules using the kernel module container..."
     for module in ${KMOD_NAMES}; do
 
-        kabi_check_module "${module}.ko"
+        kabi_check_module "${module}"
 
         if is_kmod_loaded "${module}"; then
             echo "Kernel module ${module} already loaded"
         else
             module=${module//-/_} # replace any dashes with underscore
             # TODO kvc_c_run --privileged $IMAGE modprobe ${module}
+            rmmod "${module}"
             modprobe "${module}"
         fi
     done
